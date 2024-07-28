1224cba92a68f89da9190d1a5f425b8fc6752cd8
mahimakukadiya <kukadiyamahimace@gmail.com>
2024-07-28 11:37:51 +0530

    Add files via upload

---
+++ P2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int lenght=50,breadth=30;
+    int area=lenght*breadth;
+    printf("The area is:%d",lenght*breadth);
+    int perimeter=(lenght+breadth)*2;
+    printf("\nThe perimeter is:%d",(lenght+breadth)*2);
+    printf("\nmahima kukadiya, 24TCES9M");
+}
