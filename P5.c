e96586b1aed3c3f0fa592c00f88811dbed754c01
mahimakukadiya <kukadiyamahimace@gmail.com>
2024-07-28 12:00:56 +0530

    Add files via upload

---
+++ P5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float Celsius;
+    float Fahrenheit;
+    printf("Enter The Value Of Celsius:", Celsius);
+    scanf("%f",&Celsius);
+    Fahrenheit=(Celsius*9/5)+32;
+    printf("Answer of Fahrenheit:%f",Fahrenheit);
+    printf("\nmahima kukadiya, 24TCES9M");
+}
