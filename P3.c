756e66ad11f32bf5d24185bf2ccbccc7155a43be
mahimakukadiya <kukadiyamahimace@gmail.com>
2024-07-28 12:00:16 +0530

    Add files via upload

---
+++ P3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float principal=50000;
+    float rate=5.5;
+    float time=3;
+    float Simple_interest=(principal*rate*time)/100;
+    printf("principal Ammount:%f\n",principal);
+    printf("Annual interest rate:%f\n",rate);
+    printf("time period:%f\n",time);
+    printf("Simple_interest:%f\n",Simple_interest);
+    printf("Mahima kukadiya,24TCES9M.");
+}
