75ec6b2de81c58504f9e0ec537a7d3ef97575647
mahimakukadiya <kukadiyamahimace@gmail.com>
2024-08-06 10:31:13 +0530

    Add files via upload

---
+++ P14(ii).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int rows;
+    printf("enter size of table:");
+    scanf("%d",&rows);
+    for(int i=0; i<=rows; i++)
+    {
+        for(int j=0; j<=2*(rows-i)-1; j++)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=i; k++)
+        {
+            printf("%d ",k);
+        }
+        printf("\n");
+    }
+    return 0;
+}
