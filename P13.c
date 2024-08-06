1010a666ddd17ec6fd73c873f010f4948d81aaec
mahimakukadiya <kukadiyamahimace@gmail.com>
2024-08-06 09:45:15 +0530

    Add files via upload

---
+++ P13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int j,i,a,b;
+    printf("size of table vertically:");
+    scanf("%d",&a);
+    printf("size of table horizontal:");
+    scanf("%d",&b);
+    for(i=1;i<=a;i++){
+    for(j=1;j<=b;j++){
+    printf("%5d",i*j);}
+    printf("\n");}
+
+}
